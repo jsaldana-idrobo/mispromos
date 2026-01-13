5e74829e3fe45cbb11233a758385e3bbe3182542
Sebastian <jsaldana.idrobo@gmail.com>
2026-01-13 17:07:44 -0500

    Add Vercel adapter for Astro build

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/serverless";
 
 export default defineConfig({
   integrations: [tailwind({ applyBaseStyles: true })],
   srcDir: "src",
   output: "server",
+  adapter: vercel(),
 });
